a6ddf8412356416f085e10de59537f193c34cf8a
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2020-10-07 13:29:42 +0300

    "191. Number of 1 Bits"

---
+++ src/main/java/ru/bitmanipulation/level/easy/NumberOfOneBits.java
@@ -0,0 +1,36 @@
+package ru.bitmanipulation.level.easy;
+
+/*
+
+
+        Write a function that takes an unsigned integer and return the number of '1' bits it has (also known as the Hamming weight).
+
+
+ */
+
+/**
+ * @author Sir-Hedgehog (mailto:quaresma_08@mail.ru)
+ * @version 1.0
+ * @since 07.10.2020
+ */
+
+public class NumberOfOneBits {
+
+    /**
+     * Метод находит количество единиц в числе битового представления
+     * @param number - число
+     * @return - количество единиц
+     */
+
+    public int getNumberOfOneBits(int number) {
+        int counter = 0;
+        String stringOfBits = Integer.toBinaryString(number);
+        char[] charArray = stringOfBits.toCharArray();
+        for (char element : charArray) {
+            if (element == '1') {
+                ++counter;
+            }
+        }
+        return counter;
+    }
+}
---
+++ src/test/java/ru/bitmanipulation/level/easy/NumberOfOneBitsTest.java
@@ -0,0 +1,35 @@
+package ru.bitmanipulation.level.easy;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Sir-Hedgehog (mailto:quaresma_08@mail.ru)
+ * @version 1.0
+ * @since 07.10.2020
+ */
+
+public class NumberOfOneBitsTest {
+    @Test
+    public void checkQuantityOfThreeUnits() {
+        NumberOfOneBits numberOfOneBits = new NumberOfOneBits();
+        int result = numberOfOneBits.getNumberOfOneBits(00000000000000000000000000001011);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void checkQuantityOfOneUnit() {
+        NumberOfOneBits numberOfOneBits = new NumberOfOneBits();
+        int result = numberOfOneBits.getNumberOfOneBits(00000000000000000000000010000000);
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void checkQuantityOfNineUnits() {
+        NumberOfOneBits numberOfOneBits = new NumberOfOneBits();
+        int result = numberOfOneBits.getNumberOfOneBits(011110111101);
+        assertThat(result, is(9));
+    }
+}
